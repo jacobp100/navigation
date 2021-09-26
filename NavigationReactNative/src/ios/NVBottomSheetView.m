bc21a4f5265938357b4cbd352694686428860d61
Graham Mendick <graham.mendick@gmail.com>
2021-09-26 22:53:10 +0100

    Presented bottom sheet on move to window
    It's empty so can't see it but think it's there. Need to test with some content

--- NavigationReactNative/src/ios/NVBottomSheetView.m
+++ NavigationReactNative/src/ios/NVBottomSheetView.m
@@ -1,5 +1,27 @@
 #import "NVBottomSheetView.h"
 
-@implementation NVBottomSheetView
+#import <React/UIView+React.h>
+
+@implementation NVBottomSheetView {
+    UIViewController *_bottomSheetController;
+}
+
+- (id)init
+{
+    if (self = [super init]) {
+        _bottomSheetController = [UIViewController new];
+    }
+    return self;
+}
+
+- (void)didMoveToWindow
+{
+    [super didMoveToWindow];
+    if (@available(iOS 15.0, *)) {
+        UISheetPresentationController *sheet =_bottomSheetController.sheetPresentationController;
+        sheet.detents = [NSArray arrayWithObjects:[UISheetPresentationControllerDetent mediumDetent], [UISheetPresentationControllerDetent largeDetent], nil];
+    }
+    [self.reactViewController presentViewController:_bottomSheetController animated:true completion:^{}];
+}
 
 @end
