daaf9bf7d7990eac939daf47a7915726853a9cc1
Jacob Parker <jacob.parker@tesco.com>
2021-10-01 12:56:11 +0100

    wip

--- NavigationReactNative/sample/twitter/Home.js
+++ NavigationReactNative/sample/twitter/Home.js
@@ -1,14 +1,37 @@
 import React from 'react';
-import {Platform, View} from 'react-native';
-import {NavigationBar, CoordinatorLayout, BottomSheet} from 'navigation-react-native';
+import {Platform, View, Text} from 'react-native';
+import {
+  NavigationBar,
+  CoordinatorLayout,
+  BottomSheet,
+} from 'navigation-react-native';
 import Tweets from './Tweets';
 
-export default ({tweets}) => (
-  <CoordinatorLayout>
-    <NavigationBar title="Home" barTintColor={Platform.OS === 'android' ? '#fff' : 'rgb(247,247,247)'} />
-    <Tweets tweets={tweets} />
-    <BottomSheet>
-      <View style={{flex: 1, backgroundColor: 'lightgreen'}} />
-    </BottomSheet>
-  </CoordinatorLayout>
-);
+export default ({tweets}) => {
+  const [height, setHeight] = React.useState(0);
+
+  return (
+    <CoordinatorLayout>
+      <NavigationBar
+        title="Home"
+        barTintColor={Platform.OS === 'android' ? '#fff' : 'rgb(247,247,247)'}
+      />
+      <Tweets tweets={tweets} />
+      <BottomSheet>
+        <View
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
+            backgroundColor: 'lightgreen',
+            borderWidth: 10,
+            borderColor: 'red',
+          }}
+          // onLayout={e => setHeight(e.nativeEvent.layout.height)}
+        >
+          <Text>{height}</Text>
+        </View>
+      </BottomSheet>
+    </CoordinatorLayout>
+  );
+};
